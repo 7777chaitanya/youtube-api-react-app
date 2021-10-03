ab4530f1d518f63fe780211d6b9ce8ec05608d9b
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-03 08:18:44 +0530

    Added : axios instance & .env file

---
+++ src/api/youtubeAxiosInstance.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params : {
+        part : "snippet",
+        maxResults : 5,
+        key : process.env.REACT_APP_YOUTUBE_API_KEY
+    }
+  });
