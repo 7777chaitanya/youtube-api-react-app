d1ddc26ac833b411649dfd5caf397afc20d94d8a
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-03 08:21:08 +0530

    Added : videos grid comp

---
+++ src/components/VideosGrid/VideosGrid.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import useStyles from "./styles";
+
+const VideosGrid = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            videos grid
+        </div>
+    )
+}
+
+export default VideosGrid
