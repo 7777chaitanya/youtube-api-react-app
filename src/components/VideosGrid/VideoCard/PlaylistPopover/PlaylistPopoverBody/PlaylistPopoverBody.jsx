535e8641b6ce23aa648bc7da3500d017e3aedb58
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-05 18:28:34 +0530

    Added : create playlist popover

--- src/components/VideosGrid/VideoCard/PlaylistPopover/PlaylistPopoverBody/PlaylistPopoverBody.jsx
+++ src/components/VideosGrid/VideoCard/PlaylistPopover/PlaylistPopoverBody/PlaylistPopoverBody.jsx
@@ -1,16 +1,26 @@
-import React, {useContext} from "react";
-import { Typography } from "@material-ui/core";
-import useStyles from "./styles"
-import { List, ListItem, ListItemText, Divider } from '@material-ui/core';
+import React, { useContext } from "react";
+import { Typography, Popover } from "@material-ui/core";
+import useStyles from "./styles";
+import { List, ListItem, ListItemText, Divider } from "@material-ui/core";
 import { PlaylistContext } from "../../../../../contexts/PlaylistContext";
-import addToRequiredPlaylist from "../../../../../firestoreFunctions/addToRequiredPlaylist"
+import addToRequiredPlaylist from "../../../../../firestoreFunctions/addToRequiredPlaylist";
 
-
-const PlaylistPopoverBody = ({eachVideo, handleClose1}) => {
+const PlaylistPopoverBody = ({ eachVideo, handleClose1 }) => {
   const classes = useStyles();
-  const {playlist} = useContext(PlaylistContext);
+  const { playlist } = useContext(PlaylistContext);
 
-  
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
   return (
     <div>
@@ -21,18 +31,41 @@ const PlaylistPopoverBody = ({eachVideo, handleClose1}) => {
         <ListItem button>
           <ListItemText primary="Spam" />
         </ListItem>
-        {playlist.allPlaylists.map(eachItem => 
-            <ListItem button onClick={() => addToRequiredPlaylist(eachVideo, eachItem, handleClose1)}>
+        {playlist.allPlaylists.map((eachItem) => (
+          <ListItem
+            button
+            onClick={() =>
+              addToRequiredPlaylist(eachVideo, eachItem, handleClose1)
+            }
+          >
             <ListItemText primary={eachItem} />
           </ListItem>
-            )}
+        ))}
 
-
-        <Divider/>
-        <ListItem button>
+        <Divider />
+        <ListItem button onClick={handleClick}>
           <ListItemText primary="Create new playlist" />
         </ListItem>
       </List>
+
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <Typography className={classes.typography}>
+          The content of the Popover.
+        </Typography>
+      </Popover>
     </div>
   );
 };
