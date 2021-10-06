99c7a57dcb256dc7c9a48571a9003e407585f502
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-06 11:33:30 +0530

    Added : Theme Provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { HomePageVideosProvider } from "./contexts/HomePageVideosContext";
 import { PlaylistProvider } from "./contexts/PlaylistContext";
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light : "rgb(222,79,78)",
+      main: purple[500],
+      // dark
+
+    },
+    secondary: {
+      light : "rgb(91,96,116)",
+      main: green[500],
+    },
+  },
+});
 
 ReactDOM.render(
-  <HomePageVideosProvider>
-    <PlaylistProvider>
-    <App />
-    </PlaylistProvider>
-  </HomePageVideosProvider>,
+  <ThemeProvider theme={theme}>
+    <HomePageVideosProvider>
+      <PlaylistProvider>
+        <App />
+      </PlaylistProvider>
+    </HomePageVideosProvider>
+  </ThemeProvider>,
 
   document.getElementById("root")
 );
