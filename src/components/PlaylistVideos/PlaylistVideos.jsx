0e71c0875a5ab6deb599041520a1d1179f76f8bc
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-08 20:17:56 +0530

    Added : new logo

--- src/components/PlaylistVideos/PlaylistVideos.jsx
+++ src/components/PlaylistVideos/PlaylistVideos.jsx
@@ -2,39 +2,62 @@ import { useContext } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { PlaylistContext } from "../../contexts/PlaylistContext";
 import VideosGrid from "../VideosGrid/VideosGrid";
-import {IconButton, Typography} from "@material-ui/core";
+import { IconButton, Typography, Box } from "@material-ui/core";
 import deletePlaylist from "../../firestoreFunctions/deletePlaylist";
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-
-
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import useStyles from "./styles";
 
 const PlaylistVideos = (props) => {
   const params = useParams();
   const { playlist } = useContext(PlaylistContext);
   const playlistName = params?.playlistName;
   const history = useHistory();
+  const classes = useStyles();
 
   const handlePlaylistDelete = () => {
     deletePlaylist(playlistName);
-    history.push("/")
-  }
+    history.push("/");
+  };
+
+  const handleGoToTop = () => {
+    window.scrollTo(0, 0);
+  };
 
   console.log("Params => ", params);
   return (
     <>
       <div style={{ marginTop: "5rem" }}>
-        <Typography variant="h5">{playlistName}</Typography>
-        <IconButton onClick={handlePlaylistDelete}>
-                <DeleteForeverIcon/>
-            </IconButton>
+        <Box className={classes.headerBox}>
+          <Typography variant="h4" align="center">
+            {playlistName}
+          </Typography>
 
-            {playlist&& playlistName && 
-            (playlist[playlistName]?.length==0 ?
-            (<Typography variant="h5">There are no videos in this playlist</Typography>)
-            :
+          <IconButton onClick={handlePlaylistDelete}>
+            <DeleteForeverIcon
+              color="secondary"
+              className={classes.deleteIcon}
+            />
+          </IconButton>
+        </Box>
 
-        
-        <VideosGrid videos={playlist[playlistName]} playlistName={playlistName}/>)}
+        {playlist &&
+          playlistName &&
+          (playlist[playlistName]?.length == 0 ? (
+            <Typography variant="h5">
+              There are no videos in this playlist
+            </Typography>
+          ) : (
+            <VideosGrid
+              videos={playlist[playlistName]}
+              playlistName={playlistName}
+            />
+          ))}
+        <Box className={classes.goToTopButtonBox}>
+          <IconButton onClick={handleGoToTop}>
+            <ArrowUpwardIcon />
+          </IconButton>
+        </Box>
       </div>
 
       {/* <h1>{params?.playlistName}</h1> */}
---
+++ src/components/PlaylistVideos/styles.js
@@ -0,0 +1,37 @@
+import { makeStyles } from "@material-ui/core";
+
+const  useStyles = makeStyles(theme=>({
+    noItemsPaper: {
+        padding: "2rem",
+        [theme.breakpoints.down("sm")]: {
+          maxWidth: "80vw",
+          margin: "auto",
+        },
+        [theme.breakpoints.up("sm")]: {
+          maxWidth: "60vw",
+          margin: "auto",
+        },
+      },
+      deleteIcon:{
+        "&:hover": {
+            color: "red"
+          }
+      },
+      headerBox: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingBottom: "2rem",
+      },
+      goToTopButtonBox:{
+        position : "fixed",
+        zIndex : "100",
+        bottom : "2rem",
+        right : "2rem",
+        border : "1px solid gray",
+        borderRadius : "50px",
+        backgroundColor : "rgb(147,104,247)"
+    }
+}))
+
+export default useStyles;
