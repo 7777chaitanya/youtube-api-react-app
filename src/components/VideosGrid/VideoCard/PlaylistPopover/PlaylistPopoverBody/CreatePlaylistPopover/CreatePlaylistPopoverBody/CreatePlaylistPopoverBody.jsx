07927b7508eb070b5f1309171c7f62c08bb870c3
chaitanya7666 <chaitu.y25@gmail.com>
2021-10-09 12:46:05 +0530

    Added : disbaled submit button when input is empty

--- src/components/VideosGrid/VideoCard/PlaylistPopover/PlaylistPopoverBody/CreatePlaylistPopover/CreatePlaylistPopoverBody/CreatePlaylistPopoverBody.jsx
+++ src/components/VideosGrid/VideoCard/PlaylistPopover/PlaylistPopoverBody/CreatePlaylistPopover/CreatePlaylistPopoverBody/CreatePlaylistPopoverBody.jsx
@@ -1,32 +1,54 @@
-import React,{useRef} from "react";
-import { Typography, Box, TextField, Button } from '@material-ui/core';
+import React, { useRef, useState } from "react";
+import { Typography, Box, TextField, Button } from "@material-ui/core";
 import useStyles from "./styles";
-import createPlaylistInFirestoreAndaddVideo from "../../../../../../../firestoreFunctions/createPlaylistInFirestoreAndaddVideo"
+import createPlaylistInFirestoreAndaddVideo from "../../../../../../../firestoreFunctions/createPlaylistInFirestoreAndaddVideo";
 
+const CreatePlaylistPopoverBody = ({ eachVideo, handleClose }) => {
+  const classes = useStyles();
+  const playlistNameRef = useRef(null);
 
-const CreatePlaylistPopoverBody = ({eachVideo, handleClose}) => {
-    const classes = useStyles();
-    const playlistNameRef = useRef(null);
+  const [disabledButton, setDisabledButton] = useState(true);
 
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createPlaylistInFirestoreAndaddVideo(
+      playlistNameRef.current.value,
+      eachVideo,
+      handleClose
+    );
+  };
+
+  const handleChange = () => {
+    if (playlistNameRef.current.value.length === 0) {
+      setDisabledButton(true);
+    } else {
+      setDisabledButton(false);
+    }
+  };
+
   return (
-      
+    <form action="submit" onSubmit={handleSubmit}>
       <Box className={classes.playlistDetailsBox}>
-
-      <TextField
+        <TextField
           id="standard-password-input"
           label="Enter playlist name"
           autoComplete="current-password"
           inputRef={playlistNameRef}
           autoFocus
+          onChange={handleChange}
         />
 
-        <Button color="primary" variant="contained"
-        onClick={() => createPlaylistInFirestoreAndaddVideo(playlistNameRef.current.value,eachVideo, handleClose )}
+        <Button
+          color="primary"
+          variant="contained"
+          type="submit"
+          onClick={handleSubmit}
+          disabled={disabledButton}
         >
-            Create playlist
+          Create playlist
         </Button>
       </Box>
+    </form>
   );
 };
 
